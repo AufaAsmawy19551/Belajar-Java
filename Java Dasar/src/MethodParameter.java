1b3727f9f4d6b304ca7c86709c7078da69d78195
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2023-03-06 22:15:04 +0700

    update

---
+++ Java Dasar/src/MethodParameter.java
@@ -0,0 +1,8 @@
+public class MethodParameter {
+    public static void main(String[] args) {
+        sayHello("Aufa","Asmawy");
+    }
+    static void sayHello(String FirstName, String LastName){
+        System.out.println("Hello " + FirstName + " " + LastName);
+    }
+}
