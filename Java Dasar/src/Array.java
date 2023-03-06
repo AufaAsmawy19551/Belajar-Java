60f6598e71397e81e26330db8686eadf32192db4
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2023-03-06 22:12:42 +0700

    update

---
+++ Java Dasar/src/Array.java
@@ -0,0 +1,15 @@
+public class Array {
+    public static void main(String[] args) {
+        Integer[] angka = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println(angka[0]);
+        System.out.println(angka[1]);
+        System.out.println(angka[2]);
+        System.out.println(angka[3]);
+        System.out.println(angka[4]);
+        System.out.println(angka[5]);
+        System.out.println(angka[6]);
+        System.out.println(angka[7]);
+        System.out.println(angka[8]);
+        System.out.println(angka[9]);
+    }
+}
