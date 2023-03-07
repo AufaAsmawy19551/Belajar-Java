968561dc9aafe116ab6b7de787ce766b0094baab
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2023-03-07 22:11:25 +0700

    update

---
+++ Java Dasar/src/MethodReturnValue.java
@@ -0,0 +1,10 @@
+public class MethodReturnValue {
+    public static void main(String[] args) {
+
+        System.out.println(sum(5,9));
+    }
+    static int sum(int value1, int value2){
+
+        return value1 + value2;
+    }
+}
