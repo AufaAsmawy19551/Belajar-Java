66581b120237f8c4a0a0a365e4b72fc27a62f565
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2023-03-10 21:50:30 +0700

    update

---
+++ Java Dasar/src/test/BresenhamLine2.java
@@ -0,0 +1,19 @@
+package test;
+
+public class BresenhamLine2 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+//        System.out.println("a = " + a);
+//        System.out.println("b = " + b);
+//        System.out.println();
+
+        int tmp = a;
+        a = b;
+        b = tmp;
+
+//        System.out.println("a = " + a);
+//        System.out.println("b = " + b);
+    }
+}
