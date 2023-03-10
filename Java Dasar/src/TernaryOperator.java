484167d79f047a2ba8e66304de0578f1a7b18d05
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2023-03-10 21:46:45 +0700

    update

---
+++ Java Dasar/src/TernaryOperator.java
@@ -0,0 +1,7 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+        var nilai = 75;
+        String hasil = nilai >= 70? "Anda Tidak Remidi" : "Anda Remidi";
+        System.out.println(hasil);
+    }
+}
