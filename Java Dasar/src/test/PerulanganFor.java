a48e69e170dac141224316b3c17213f161d94441
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2023-03-13 16:52:18 +0700

    update

---
+++ Java Dasar/src/test/PerulanganFor.java
@@ -0,0 +1,53 @@
+package test;
+
+public class PerulanganFor {
+    public static void main(String[] args) {
+        //for (initial counter; batas perulanagan; increment){
+        //      Statement;
+        //      Statement;
+        //      Statement;
+        //      Statement;
+        // }
+
+//        for (int i = 0; i < 10; i++){
+//
+//            System.out.println("PErulangan ke-" + i);
+//        }
+
+//        for(int i = 5; i < 23; i += 3){
+//            System.out.println(i);
+//
+//        }
+
+        for (int i = 0; i < 100; i++){
+            if (i == 50 || i == 70){
+                continue;
+            }
+            System.out.println(i);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+//        Integer[] numbers = {2, 4, 6, 9, 12, 15, 18, 21};
+//
+//        for(int i = 0; i < numbers.length; i++){
+////            System.out.println(i);
+//
+//            System.out.println(numbers[i]);
+//        }
+
+//        for (int i = 0; i < 20; i += 2){
+//
+//            System.out.println(i);
+//        }
+    }
+}
