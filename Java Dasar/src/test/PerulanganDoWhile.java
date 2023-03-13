b915797230a37fdd3d15f6adb3621ffda6095c48
AufaAsmawy19551 <aufamuhammad19551@gmail.com>
2023-03-13 16:51:58 +0700

    update

---
+++ Java Dasar/src/test/PerulanganDoWhile.java
@@ -0,0 +1,13 @@
+package test;
+
+public class PerulanganDoWhile {
+    public static void main(String[] args) {
+        Integer angka = 100;
+
+        do {
+            angka -= 1;
+            System.out.println(angka);
+        } while (angka > 10);
+    }
+
+}
